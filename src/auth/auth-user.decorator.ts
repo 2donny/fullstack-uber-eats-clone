0a96aca74b7c1d3bc6c26da65f2cbc334a864219
EdonJung <2donny@naver.com>
2021-06-17 17:19:00 +0900

    custom auth decorator

---
+++ src/auth/auth-user.decorator.ts
@@ -0,0 +1,11 @@
+import { createParamDecorator } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+export const AuthUser = createParamDecorator(
+  (data: any, ctx: GqlExecutionContext) => {
+    const gqlCtx = GqlExecutionContext.create(ctx).getContext();
+    const { user } = gqlCtx;
+    console.log(user);
+    return user;
+  },
+);
