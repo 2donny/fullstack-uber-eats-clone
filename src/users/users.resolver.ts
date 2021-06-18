48a1659a50999a4f1a2b47cd16769f129ed15ec4
EdonJung <2donny@naver.com>
2021-06-18 21:01:30 +0900

    editProfile, verifyEmail

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,15 +1,18 @@
-import { Args, Context, Mutation, Query } from '@nestjs/graphql';
+import { Args, Mutation, Query } from '@nestjs/graphql';
 import { Resolver } from '@nestjs/graphql';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 import {
   CreateAccountInput,
   CreateAccountOutput,
-} from './dtos/createAccount.dto';
+} from './dtos/create-account.dto';
 import { LoginInput, LoginOutput } from './dtos/login.dto';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { AuthUser } from 'src/auth/auth-user.decorator';
+import { UserProfileInput, UserProfileOutput } from './dtos/user-profile.dto';
+import { EditProfileInput, EditProfileOutput } from './dtos/edit-profile.dto';
+import { VerifyEmailInput, VerifyEmailOutput } from './dtos/verify-email.dto';
 
 @Resolver(() => User)
 export class UsersResolver {
@@ -47,5 +50,54 @@ export class UsersResolver {
   @UseGuards(AuthGuard)
   async me(@AuthUser() authUser: User) {
     return authUser;
+  }
+
+  @Query(() => UserProfileOutput)
+  @UseGuards(AuthGuard)
+  async userProfile(
+    @Args() userProfileInput: UserProfileInput,
+  ): Promise<UserProfileOutput> {
+    try {
+      const user = await this.usersService.findById(userProfileInput.id);
+      if (!user) {
+        throw Error();
+      }
+      return {
+        ok: true,
+        user,
+      };
+    } catch {
+      return {
+        ok: false,
+        error: '존재하지 않는 계정입니다.',
+      };
+    }
+  }
+
+  @Mutation(() => EditProfileOutput)
+  @UseGuards(AuthGuard)
+  async editProfile(
+    @AuthUser() authUser: User,
+    @Args('input') editProfileInput: EditProfileInput,
+  ): Promise<EditProfileOutput> {
+    try {
+      await this.usersService.editProfile(authUser.id, editProfileInput);
+      return {
+        ok: true,
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        ok: false,
+        error,
+      };
+    }
+  }
+
+  @Mutation(() => VerifyEmailOutput)
+  async verifyEmail(
+    @Args('input') { code }: VerifyEmailInput,
+  ): Promise<VerifyEmailOutput> {
+    return this.usersService.verifyEmail(code);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,38 +1,47 @@
 import {
   CreateAccountInput,
   CreateAccountOutput,
-} from './dtos/createAccount.dto';
+} from './dtos/create-account.dto';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { LoginInput, LoginOutput } from './dtos/login.dto';
-import * as jwt from 'jsonwebtoken';
-import { ConfigService } from '@nestjs/config';
 import { JwtService } from 'src/jwt/jwt.service';
+import { EditProfileInput } from './dtos/edit-profile.dto';
+import { Verification } from './entities/verification.entity';
+import { VerifyEmailOutput } from './dtos/verify-email.dto';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private readonly users: Repository<User>,
-    private readonly config: ConfigService,
+    @InjectRepository(Verification)
+    private readonly verificationService: Repository<Verification>,
     private readonly jwtService: JwtService,
   ) {}
 
   async createAccount(
-    createAccountDto: CreateAccountInput,
+    createAccountInput: CreateAccountInput,
   ): Promise<CreateAccountOutput> {
-    const { email, password, role } = createAccountDto;
+    const { email, password, role } = createAccountInput;
     try {
-      const user = await this.users.findOne({ email });
-      if (user) {
+      const exists = await this.users.findOne({ email });
+      if (exists) {
         return {
           ok: false,
           error: '이미 존재하는 계정입니다.',
         };
       }
-      await this.users.save(this.users.create({ email, password, role }));
+      const user = await this.users.save(
+        this.users.create({ email, password, role }),
+      );
+      await this.verificationService.save(
+        this.verificationService.create({
+          user,
+        }),
+      );
       return { ok: true };
     } catch (error) {
       return {
@@ -45,7 +54,10 @@ export class UsersService {
   async login(loginInput: LoginInput): Promise<LoginOutput> {
     const { email, password } = loginInput;
     try {
-      const user = await this.users.findOne({ email });
+      const user = await this.users.findOne(
+        { email },
+        { select: ['password', 'id'] },
+      );
       if (!user) {
         return {
           ok: false,
@@ -62,6 +74,7 @@ export class UsersService {
       const token = this.jwtService.sign({ id: user.id });
       return { ok: true, token };
     } catch (error) {
+      console.log(error);
       return {
         ok: false,
         error,
@@ -71,5 +84,38 @@ export class UsersService {
 
   async findById(id: number): Promise<User> {
     return this.users.findOne({ id });
+  }
+
+  async editProfile(
+    userId: number,
+    { email, password, role }: EditProfileInput,
+  ) {
+    const user = await this.users.findOne({ id: userId });
+    if (email) {
+      user.email = email;
+      user.verified = false;
+    }
+    if (password) user.password = password;
+    if (role) user.role = role;
+    return this.users.save(user);
+  }
+
+  async verifyEmail(code: string): Promise<VerifyEmailOutput> {
+    const verification = await this.verificationService.findOne(
+      { code },
+      { relations: ['user'] },
+    );
+    if (verification?.user) {
+      verification.user.verified = true;
+      this.users.save(verification.user);
+      return {
+        ok: true,
+      };
+    } else {
+      return {
+        ok: false,
+        error: '유효하지 않는 코드입니다.',
+      };
+    }
   }
 }
