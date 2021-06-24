3b80a04d7a9831edf9bbcc76272cd470ce94f968
EdonJung <2donny@naver.com>
2021-06-25 02:04:14 +0900

    mail module

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,22 +1,57 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
-import { MailModuleOptions } from './mail.interface';
+import { EmailVar, MailModuleOptions } from './mail.interface';
+import * as mailgun from 'mailgun-js';
 
 @Injectable()
 export class MailService {
   constructor(
-    @Inject(CONFIG_OPTIONS)
-    private readonly options: MailModuleOptions,
-  ) {
-    console.log(options);
-  }
+    @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
+  ) {}
 
-  async sendUserConfirmation(subject: string, email: string, code: string) {}
+  private async sendUserConfirmation(
+    subject: string,
+    template,
+    emailVars: EmailVar[],
+  ) {
+    const mg = mailgun({
+      apiKey: this.options.apiKey,
+      domain: this.options.domain,
+    });
+
+    const data = {
+      from: 'Xircle support <support@xircle.org>',
+      to: `2donny@naver.com`,
+      subject,
+      template,
+    };
+
+    emailVars.forEach((Evar) => {
+      data[`v:${Evar.key}`] = Evar.value;
+    });
+
+    try {
+      mg.messages().send(data, function (error, body) {
+        console.log(body, error);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
   async sendVerificationEmail(email: string, code: string) {
     await this.sendUserConfirmation(
       '✅ 안전하게 인증하고 🚀 써클에서 새로운 네트워킹을 경험하세요! ',
-      email,
-      code,
+      'confirmation',
+      [
+        {
+          key: 'code',
+          value: code,
+        },
+        {
+          key: 'email',
+          value: email,
+        },
+      ],
     );
   }
 }
