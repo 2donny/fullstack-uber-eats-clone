5fb427c57de66f95b4990d04d731a36dc5590c03
EdonJung <2donny@naver.com>
2021-06-29 23:14:21 +0900

    edit restaurant

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,9 +1,17 @@
+import { SetMetadata, UseGuards } from '@nestjs/common';
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { AuthUser } from 'src/auth/auth-user.decorator';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { Roles } from 'src/auth/role.decorator';
+import { User, UserRole } from 'src/users/entities/user.entity';
 import {
   CreateRestaurantInput,
   CreateRestaurantOutput,
 } from './dto/create-restaurant.dto';
-import { UpdateRestaurantInput } from './dto/update-restaurant.dto';
+import {
+  EditRestaurantInput,
+  EditRestaurantOutput,
+} from './dto/edit-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 import { RestaurantsService } from './restaurants.service';
 
@@ -12,9 +20,20 @@ export class RestaurantsResolver {
   constructor(private readonly restaurantService: RestaurantsService) {}
 
   @Mutation(() => CreateRestaurantOutput)
-  async createRestaurant(
+  @Roles(['Owner'])
+  public async createRestaurant(
+    @AuthUser() authUser: User,
     @Args('input') createRestaurant: CreateRestaurantInput,
-  ) {
-    console.log(createRestaurant);
+  ): Promise<CreateRestaurantOutput> {
+    return this.restaurantService.createRestaurant(authUser, createRestaurant);
+  }
+
+  @Mutation(() => EditRestaurantOutput)
+  @Roles(['Owner'])
+  public async editRestaurant(
+    @AuthUser() authUser: User,
+    @Args('data') editRestaurantInput: EditRestaurantInput,
+  ): Promise<EditRestaurantOutput> {
+    return this.restaurantService.editRestaurant(authUser, editRestaurantInput);
   }
 }
--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
-import { CreateRestaurantInput, CreateRestaurantOutput } from './dto/create-restaurant.dto';
-import { UpdateRestaurantInput } from './dto/update-restaurant.dto';
+import {
+  CreateRestaurantInput,
+  CreateRestaurantOutput,
+} from './dto/create-restaurant.dto';
+import {
+  EditRestaurantInput,
+  EditRestaurantOutput,
+} from './dto/edit-restaurant.dto';
+import { Category } from './entities/category.entity';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Injectable()
@@ -10,9 +18,90 @@ export class RestaurantsService {
   constructor(
     @InjectRepository(Restaurant)
     private readonly restaurants: Repository<Restaurant>,
+    @InjectRepository(Category)
+    private readonly categories: Repository<Category>,
   ) {}
 
-  async createRestaurant(createRestaurantInput: CreateRestaurantInput) {
+  public async getOrCreateCategory(name: string): Promise<Category> {
+    const categoryName = name.trim().toLowerCase().replace(/ +/g, ' ');
+    const categorySlug = categoryName.replace(/ /g, '-');
+    let category = await this.categories.findOne({
+      where: { slug: categorySlug },
+    });
+    if (!category) {
+      category = await this.categories.save(
+        this.categories.create({
+          slug: categorySlug,
+        }),
+      );
+    }
+    return category;
+  }
 
+  public async createRestaurant(
+    owner: User,
+    createRestaurantInput: CreateRestaurantInput,
+  ): Promise<CreateRestaurantOutput> {
+    try {
+      const category = await this.getOrCreateCategory(
+        createRestaurantInput.categoryName,
+      );
+      const newRestaurant = this.restaurants.create({
+        ...createRestaurantInput,
+        owner,
+        category,
+      });
+      await this.restaurants.save(newRestaurant);
+
+      return {
+        ok: true,
+      };
+    } catch (error) {
+      return {
+        ok: false,
+        error: '음식점 생성을 실패했습니다',
+      };
+    }
+  }
+
+  public async editRestaurant(
+    owner: User,
+    editRestaurantInput: EditRestaurantInput,
+  ): Promise<EditRestaurantOutput> {
+    try {
+      const restaurant = await this.restaurants.findOne(
+        {
+          id: editRestaurantInput.restaurantId,
+        },
+        {
+          loadRelationIds: true,
+        },
+      );
+
+      if (!restaurant)
+        return { ok: false, error: '레스토랑이 존재하지 않습니다.' };
+
+      if (restaurant.ownerId !== owner.id) {
+        return {
+          ok: false,
+          error: '자신의 레스토랑이 아니면 편집할 수 없습니다.',
+        };
+      }
+
+      if (editRestaurantInput.categoryName) {
+        const category = await this.getOrCreateCategory(
+          editRestaurantInput.categoryName,
+        );
+      }
+
+      return {
+        ok: true,
+      };
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
   }
 }
