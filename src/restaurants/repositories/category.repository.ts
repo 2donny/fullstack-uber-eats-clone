6c92f44a97ab4deaf5cc1a0b32db42c21f9b9a64
EdonJung <2donny@naver.com>
2021-06-30 15:43:38 +0900

    Category custom repository

---
+++ src/restaurants/repositories/category.repository.ts
@@ -0,0 +1,22 @@
+import { User } from 'src/users/entities/user.entity';
+import { EntityRepository, Repository } from 'typeorm';
+import { Category } from '../entities/category.entity';
+
+@EntityRepository(Category)
+export class CategoryRepository extends Repository<Category> {
+  public async getOrCreate(name: string): Promise<Category> {
+    const categoryName = name.trim().toLowerCase().replace(/ +/g, ' ');
+    const categorySlug = categoryName.replace(/ /g, '-');
+    let category = await this.findOne({
+      where: { slug: categorySlug },
+    });
+    if (!category) {
+      category = await this.save(
+        this.create({
+          slug: categorySlug,
+        }),
+      );
+    }
+    return category;
+  }
+}
