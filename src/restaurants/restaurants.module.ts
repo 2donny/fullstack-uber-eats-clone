b5c524ab31b44952d2a767aa6a94c75c34cdb4cd
EdonJung <2donny@naver.com>
2021-07-04 22:14:03 +0900

    DI module

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Restaurant } from './entities/restaurant.entity';
-import { RestaurantsResolver } from './restaurants.resolver';
+import { CategoryRepository } from './repositories/category.repository';
+import { RestaurantRepository } from './repositories/restaurant.repository';
+import {
+  CategoryResolver,
+  RestaurantsResolver,
+  DishResolver,
+} from './restaurants.resolver';
 import { RestaurantsService } from './restaurants.service';
+import { Dish } from './entities/dish.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Restaurant])],
-  providers: [RestaurantsResolver, RestaurantsService],
+  imports: [
+    TypeOrmModule.forFeature([RestaurantRepository, CategoryRepository, Dish]),
+  ],
+  providers: [
+    RestaurantsResolver,
+    CategoryResolver,
+    DishResolver,
+    RestaurantsService,
+  ],
 })
 export class RestaurantsModule {}
